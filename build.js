93fba4171fd13f57405967ff8149bfd84a1b501c
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-01-23 08:03:27 +0100

    improved debug output

--- build.js
+++ build.js
@@ -53,7 +53,14 @@ function spawn(cmd, args, opts) {
       p.stdout.on('data', (data) => console.log(data.toString()));
       p.stderr.on('data', (data) => console.error(chalk.red(data.toString())));
     }
-    p.on('close', (code) => code == 0 ? resolve() : reject(code));
+    p.on('close', (code) => {
+      if (code === 0) {
+        resolve(code)
+      } else {
+        console.error(cmd, 'status code',code);
+        reject(`${cmd} failed with status code ${code}`);
+      }
+    });
   });
 }
 
